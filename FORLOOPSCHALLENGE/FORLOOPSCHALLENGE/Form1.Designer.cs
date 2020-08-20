d8aeb32d037dad643a957dd669393fa4fafe2d11
DeltaDizzy <ejbraswell@gmail.com>
2020-08-20 11:19:31 -0500

    for loops challenge

---
+++ FORLOOPSCHALLENGE/FORLOOPSCHALLENGE/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace FORLOOPSCHALLENGE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCount = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtBy = new System.Windows.Forms.TextBox();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCount
+            // 
+            this.btnCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCount.Location = new System.Drawing.Point(141, 270);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(144, 65);
+            this.btnCount.TabIndex = 0;
+            this.btnCount.Text = "button1";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FROM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(191, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "TO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(286, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "BY";
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFrom.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtFrom.Location = new System.Drawing.Point(52, 106);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(100, 30);
+            this.txtFrom.TabIndex = 4;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTo.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtTo.Location = new System.Drawing.Point(173, 106);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(100, 30);
+            this.txtTo.TabIndex = 5;
+            // 
+            // txtBy
+            // 
+            this.txtBy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBy.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtBy.Location = new System.Drawing.Point(292, 106);
+            this.txtBy.Name = "txtBy";
+            this.txtBy.Size = new System.Drawing.Size(100, 30);
+            this.txtBy.TabIndex = 6;
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.ItemHeight = 25;
+            this.lstNumbers.Location = new System.Drawing.Point(487, 12);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(120, 429);
+            this.lstNumbers.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 450);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.txtBy);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtBy;
+        private System.Windows.Forms.ListBox lstNumbers;
+    }
+}
+
---
+++ FORLOOPSCHALLENGE/FORLOOPSCHALLENGE/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FORLOOPSCHALLENGE
+{
+    public partial class Form1 : Form
+    {
+        int start, end, countby;
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            GetNumbers();
+
+            ShowNumbers();
+        }
+
+        public void GetNumbers()
+        {
+            start = int.Parse(txtFrom.Text);
+            end = int.Parse(txtTo.Text);
+            countby = int.Parse(txtBy.Text);
+        }
+
+        public void ShowNumbers()
+        {
+            for (int i = start; i <= end; i+=countby)
+            {
+                lstNumbers.Items.Add(i);
+                wait(1000);
+            }
+        }
+
+        public void wait(int milliseconds)
+        {
+            var timer = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0)
+            {
+                return;
+            }
+            timer.Interval = milliseconds;
+            timer.Enabled = true;
+            timer.Start();
+
+            timer.Tick += (s, e) =>
+            {
+                timer.Enabled = false;
+                timer.Stop();
+            };
+
+            while (timer.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
