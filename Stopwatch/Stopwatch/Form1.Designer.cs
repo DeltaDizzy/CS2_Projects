a6133f0bf4b83a3e2aabdef69bcb792a46edc6ce
DeltaDizzy <ejbraswell@gmail.com>
2020-09-14 10:26:49 -0500

    add stopwatch

---
+++ Stopwatch/Stopwatch/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Stopwatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tmrTenth = new System.Windows.Forms.Timer(this.components);
+            this.lblTenth = new System.Windows.Forms.Label();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.lblMinute = new System.Windows.Forms.Label();
+            this.lstSplits = new System.Windows.Forms.ListBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbPic = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tmrTenth
+            // 
+            this.tmrTenth.Tick += new System.EventHandler(this.tmrTenth_Tick);
+            // 
+            // lblTenth
+            // 
+            this.lblTenth.AutoSize = true;
+            this.lblTenth.BackColor = System.Drawing.Color.Transparent;
+            this.lblTenth.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenth.Location = new System.Drawing.Point(186, 154);
+            this.lblTenth.Name = "lblTenth";
+            this.lblTenth.Size = new System.Drawing.Size(35, 38);
+            this.lblTenth.TabIndex = 1;
+            this.lblTenth.Text = "0";
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.BackColor = System.Drawing.Color.Transparent;
+            this.lblSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeconds.Location = new System.Drawing.Point(121, 154);
+            this.lblSeconds.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(62, 38);
+            this.lblSeconds.TabIndex = 3;
+            this.lblSeconds.Text = "00:";
+            // 
+            // lblMinute
+            // 
+            this.lblMinute.AutoSize = true;
+            this.lblMinute.BackColor = System.Drawing.Color.Transparent;
+            this.lblMinute.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinute.Location = new System.Drawing.Point(53, 154);
+            this.lblMinute.Name = "lblMinute";
+            this.lblMinute.Size = new System.Drawing.Size(62, 38);
+            this.lblMinute.TabIndex = 4;
+            this.lblMinute.Text = "00:";
+            // 
+            // lstSplits
+            // 
+            this.lstSplits.FormattingEnabled = true;
+            this.lstSplits.ItemHeight = 16;
+            this.lstSplits.Location = new System.Drawing.Point(264, 12);
+            this.lstSplits.Name = "lstSplits";
+            this.lstSplits.Size = new System.Drawing.Size(225, 212);
+            this.lstSplits.TabIndex = 6;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Stopwatch.Properties.Resources.buttonstopRight;
+            this.pictureBox2.Location = new System.Drawing.Point(193, 36);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(57, 59);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Stopwatch.Properties.Resources.buttonstopleft;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(57, 59);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pbPic
+            // 
+            this.pbPic.Image = ((System.Drawing.Image)(resources.GetObject("pbPic.Image")));
+            this.pbPic.InitialImage = null;
+            this.pbPic.Location = new System.Drawing.Point(12, 12);
+            this.pbPic.Name = "pbPic";
+            this.pbPic.Size = new System.Drawing.Size(246, 308);
+            this.pbPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPic.TabIndex = 0;
+            this.pbPic.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::Stopwatch.Properties.Resources.buttonstop;
+            this.pictureBox3.Location = new System.Drawing.Point(110, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(55, 54);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 333);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lstSplits);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblMinute);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.lblTenth);
+            this.Controls.Add(this.pbPic);
+            this.Name = "Form1";
+            this.Text = "Stopwatch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer tmrTenth;
+        private System.Windows.Forms.Label lblTenth;
+        private System.Windows.Forms.PictureBox pbPic;
+        private System.Windows.Forms.Label lblSeconds;
+        private System.Windows.Forms.Label lblMinute;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox lstSplits;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Stopwatch/Stopwatch/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stopwatch
+{
+    public partial class Form1 : Form
+    {
+        int tenth,second,minute = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) // start/stop
+        {
+            tmrTenth.Enabled = !tmrTenth.Enabled;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e) // split
+        {
+            string time = $"{lblMinute.Text}{lblSeconds.Text}{lblTenth.Text}";
+            lstSplits.Items.Add(time);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            lstSplits.Items.Clear();
+            tenth = 0;
+            minute = 0;
+            second = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmrTenth_Tick(object sender, EventArgs e)
+        {
+            lblTenth.Text = tenth.ToString();
+            tenth++;
+            if (tenth == 9)
+            {
+                tenth = 0;
+                second++;
+                lblSeconds.Text = second.ToString("0#:");
+            }
+            if (second == 9)
+            {
+                second = 0;
+                minute++;
+                lblMinute.Text = minute.ToString("0#:");
+            }
+        }
+    }
+}
