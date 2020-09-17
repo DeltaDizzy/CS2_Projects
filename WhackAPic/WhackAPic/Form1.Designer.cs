bdf7d2128404aeb84918ec3bb72f6b63439ddb89
DeltaDizzy <ejbraswell@gmail.com>
2020-09-17 10:28:28 -0500

    pics

---
+++ WhackAPic/WhackAPic/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WhackAPic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbMole = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblCountdown = new System.Windows.Forms.Label();
+            this.tmrGameTimer = new System.Windows.Forms.Timer(this.components);
+            this.tmrStarter = new System.Windows.Forms.Timer(this.components);
+            this.lblStarter = new System.Windows.Forms.Label();
+            this.tmrMover = new System.Windows.Forms.Timer(this.components);
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMole)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbMole
+            // 
+            this.pbMole.Image = global::WhackAPic.Properties.Resources.gopher;
+            this.pbMole.Location = new System.Drawing.Point(0, 0);
+            this.pbMole.Name = "pbMole";
+            this.pbMole.Size = new System.Drawing.Size(117, 113);
+            this.pbMole.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbMole.TabIndex = 0;
+            this.pbMole.TabStop = false;
+            this.pbMole.Click += new System.EventHandler(this.pbMole_Click);
+            this.pbMole.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbMole_MouseDown);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.lblScore);
+            this.panel1.Controls.Add(this.lblCountdown);
+            this.panel1.Location = new System.Drawing.Point(0, 354);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 98);
+            this.panel1.TabIndex = 3;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Poplar Std", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(352, 39);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(83, 39);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "Score:";
+            // 
+            // lblCountdown
+            // 
+            this.lblCountdown.AutoSize = true;
+            this.lblCountdown.Font = new System.Drawing.Font("Poplar Std", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountdown.Location = new System.Drawing.Point(12, 39);
+            this.lblCountdown.Name = "lblCountdown";
+            this.lblCountdown.Size = new System.Drawing.Size(137, 39);
+            this.lblCountdown.TabIndex = 0;
+            this.lblCountdown.Text = "Countdown:";
+            // 
+            // tmrGameTimer
+            // 
+            this.tmrGameTimer.Interval = 1000;
+            this.tmrGameTimer.Tick += new System.EventHandler(this.tmrGameTimer_Tick);
+            // 
+            // tmrStarter
+            // 
+            this.tmrStarter.Interval = 1000;
+            this.tmrStarter.Tick += new System.EventHandler(this.tmrStarter_Tick);
+            // 
+            // lblStarter
+            // 
+            this.lblStarter.AutoSize = true;
+            this.lblStarter.Font = new System.Drawing.Font("Poplar Std", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStarter.Location = new System.Drawing.Point(258, 167);
+            this.lblStarter.Name = "lblStarter";
+            this.lblStarter.Size = new System.Drawing.Size(32, 39);
+            this.lblStarter.TabIndex = 2;
+            this.lblStarter.Text = "3";
+            // 
+            // tmrMover
+            // 
+            this.tmrMover.Tick += new System.EventHandler(this.tmrMover_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 450);
+            this.Controls.Add(this.lblStarter);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pbMole);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMole)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbMole;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblCountdown;
+        private System.Windows.Forms.Timer tmrGameTimer;
+        private System.Windows.Forms.Timer tmrStarter;
+        private System.Windows.Forms.Label lblStarter;
+        private System.Windows.Forms.Timer tmrMover;
+        private System.Windows.Forms.Timer tmr;
+    }
+}
+
---
+++ WhackAPic/WhackAPic/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhackAPic
+{
+    public partial class Form1 : Form
+    {
+        int startcountdown = 3;
+        int gametimer = 30;
+        Random r;
+        int score = 0;
+        int scoreKernel = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // gopher = mole
+        // gem = rare
+        // granite = common
+        // coal = med
+
+        private void tmrStarter_Tick(object sender, EventArgs e)
+        {
+            if (startcountdown <= 0)
+            {
+                tmrStarter.Enabled = false;
+                tmrGameTimer.Enabled = true;
+                tmrMover.Enabled = true;
+                lblStarter.Visible = false;
+                lblStarter.Location = new Point(1000, 1000);
+                return;
+            }
+            startcountdown -= 1;
+            lblStarter.Text = startcountdown.ToString();
+
+        }
+
+        private void StartCountdown()
+        {
+            tmrStarter.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartCountdown();
+            r = new Random();
+        }
+
+        private void tmrGameTimer_Tick(object sender, EventArgs e)
+        {
+            gametimer -= 1;
+            lblCountdown.Text = $"Countdown: {gametimer}";
+            if (gametimer == 0)
+            {
+                EndGame();
+            }
+            
+        }
+
+        private void UpdateImage()
+        {
+            int picselect = r.Next(0, 3);
+            switch (picselect)
+            {
+                case 0:
+                    scoreKernel += 1;
+                    pbMole.Image = Properties.Resources.granite;
+                    break;
+                case 1:
+                    scoreKernel += 2;
+                    pbMole.Image = Properties.Resources.coal;
+                    break;
+                case 2:
+                    scoreKernel += 5;
+                    pbMole.Image = Properties.Resources.gem;
+                    break;
+                default:
+                    break;
+            }
+            int x = r.Next(0, Width - pbMole.Width);
+            int y = r.Next(0, Height - pbMole.Height - panel1.Height);
+            pbMole.Location = new Point(x, y);
+        }
+
+        private void EndGame()
+        {
+            tmrGameTimer.Enabled = false;
+            tmrMover.Enabled = false;
+            lblStarter.Location = new Point(100, 100);
+            lblStarter.Text = "Game Over!";
+        }
+
+        private void pbMole_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void tmrMover_Tick(object sender, EventArgs e)
+        {
+            UpdateImage();
+            tmrMover.Interval = r.Next(5, 21)*100;
+            
+        }
+
+        private void pbMole_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (tmrGameTimer.Enabled)
+            {
+                score += scoreKernel;
+                lblScore.Text = $"Score: {score}";
+            }
+        }
+    }
+}
